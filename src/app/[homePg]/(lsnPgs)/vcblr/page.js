3ce33f6d1b3cf60732023c8d934a2201f2d823ae
IDerevyansky <igor.derevyansky@gmail.com>
2024-09-02 22:06:32 -0700

    -

--- src/app/[homePg]/(lsnPgs)/vcblr/page.js
+++ src/app/[homePg]/(lsnPgs)/vcblr/page.js
@@ -4,8 +4,34 @@ import './vcblr.css';
 export default function vcblr({ params }) {
 
   return (
-    <main className="main center-content">
-      <p className="test">qwe</p>
+    <main className="content-box">
+
+      <div className="header">
+        <div className="back"><p>Back</p></div>
+        <div className="title"><h3>Title</h3></div>
+      </div>
+
+      <div className="content">
+
+        <div className="cards">
+cards
+        </div>
+
+        <div className="count-cards">
+12|200
+        </div>
+
+        <div className="list-words">
+          
+        </div>
+
+      </div>
+
+      <div className="sheet">
+        <div className="btn-line"></div>
+
+      </div>
+
     </main>
   );
   
