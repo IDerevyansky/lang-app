b5cf8a1f2bc048723610b3ec5e8f3ec1f398f6af
IDerevyansky <igor.derevyansky@gmail.com>
2024-09-11 02:44:48 -0700

    -

--- src/app/[homePg]/(lsnPgs)/vcblr/page.js
+++ src/app/[homePg]/(lsnPgs)/vcblr/page.js
@@ -84,22 +84,58 @@ export default function vcblr({ params }) {
   useEffect(() => {
 
     let listWords = document.getElementsByClassName('list-words');
-    let centerListWords = listWords[0].clientWidth/2; //191
+    //Center div list-words
+    let ListWordsCenter = listWords[0].clientWidth/2;
+    //Offset div list-words
+    let ListWordsOffset = listWords[0].offsetLeft;
+
+
+
+    //change to loop FOR 
+    let word2 = document.getElementById(2);
+    let word2Offeset = word2.offsetLeft;
+    let word2Width = word2.clientWidth; 
+
+    console.log( listWords[0].children.length );
+
+    //set listener events to each [id]
+    for (let i = 0; i < listWords[0].children.length; i++) {
+    
+      //document.getElementById(i).addEventListener('', ); <-- Set function with listener  to events touchMove/Scroll/Click/touchStart
+ 
+      //Example
+      // function startup() {
+      //   const el = document.getElementById("canvas");
+      //   el.addEventListener("touchstart", handleStart);
+      //   el.addEventListener("touchend", handleEnd);
+      //   el.addEventListener("touchcancel", handleCancel);
+      //   el.addEventListener("touchmove", handleMove);
+      //   log("Initialized.");
+      // }
+      
+      // document.addEventListener("DOMContentLoaded", startup);
+
+
+    }
+
+    //Set listen width window
+    window.addEventListener('resize', ()=>{
+
+    //Update value centerListWords
+    ListWordsCenter = listWords[0].clientWidth/2;
+
+    } );
+
+
 
     listWords[0].scroll(
       {
         // top: 100,
-        left: 192-207+139/2,
+        left: word2Offeset - ListWordsOffset - ListWordsCenter + word2Width/2,
         behavior: "smooth",
       }
     );
 
-
-    // listWords[0].style.paddingLeft = centerListWords + 'px';
-    // listWords[0].style.paddingRight = centerListWords + 'px';
-    console.log(listWords[0].children);
-
-    let count = 0; 
 
     listWords[0].addEventListener(
       'touchmove', 
@@ -107,19 +143,17 @@ export default function vcblr({ params }) {
 
         let pageX = Math.round(e.changedTouches[0].pageX);
 
-        // console.log(pageX);
+        // console.log(e);
 
       }, { passive: true }
     );
 
 
-    // Tip:
-    // 1.Перебираем массив с данными
-    // 2.Передаем цифру в scrollTo()
-    // или
-    // 3.Передаем значение в div margin left/right
-    // или 
-    // 4.Попробовать анкерные ссылки <- try is govno
+  // Tip:-------------------------
+  //  listen:
+  //  - Resize -> Controll page center
+  //  - Touch/Scroll -> Math search a div_world with min value and make the div centred position
+  //  - Click/Tap -> Math after click we have [id] div_world and make the div ctntred position 
 
    
 
